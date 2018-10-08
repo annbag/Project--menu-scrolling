9657213d4bccd4feab739b4d007bcadfc78598a0
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-10-08 16:48:18 +0200

    add task

---
+++ script.js
@@ -0,0 +1,20 @@
+$('.me').on('click', function () {
+	$('body, html').animate({
+		scrollTop: $('.s1').offset().top
+	}, 500)
+})
+$('.portfolio').on('click', function () {
+	$('body, html').animate({
+		scrollTop: $('.s2').offset().top
+	}, 500)
+})
+$('.offer').on('click', function () {
+	$('body, html').animate({
+		scrollTop: $('.s3').offset().top
+	}, 500)
+})
+$('.contact').on('click', function () {
+	$('body, html').animate({
+		scrollTop: $('.s4').offset().top
+	}, 500)
+})
